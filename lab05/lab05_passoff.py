37e77807a61d9bb46a686e327643ebb7cbb7383e
Mike Wirthlin <wirthlin@byu.edu>
2025-01-14 21:14:53 -0700

    lab05/lab05_passoff.py

--- lab05/lab05_passoff.py
+++ lab05/lab05_passoff.py
@@ -15,12 +15,12 @@ import repo_test
 def main():
     # Check on vivado
     tester = test_suite_320.build_test_suite_320("lab05", start_date="02/10/2025")
-    tester.add_required_tracked_files(["seven_segment.sv", "sevent_segment_top.sv", "sim_seven_segment_top.png", 
+    tester.add_required_tracked_files(["seven_segment.sv", "seven_segment_top.sv", "sim_seven_segment_top.png", 
                                        "segment_lut.png"])
     tester.add_Makefile_rule("sim_sevensegment", ["seven_segment.sv"], ["sim_sevensegment.log"])
     tester.add_build_test(repo_test.file_regex_check("sim_sevensegment.log", "\*\*\* Error", "Testbench Log Test"))
     tester.add_Makefile_rule("synth", [], ["synthesis.log", "seven_segment_synth.dcp"])
-    tester.add_Makefile_rule("implement", ["sevent_segment_synth.dcp"], ["implement.log", "seven_segment.bit", 
+    tester.add_Makefile_rule("implement", ["seven_segment_synth.dcp"], ["implement.log", "seven_segment.bit", 
                                             "seven_segment.dcp", "utilization.rpt"])
     tester.run_tests()
 
