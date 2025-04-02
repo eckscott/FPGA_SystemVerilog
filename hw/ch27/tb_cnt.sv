a3be53f8dda8bba2084b0642faaeaef8d21c073b
eckscott <156133501+eckscott@users.noreply.github.com>
2025-04-02 15:43:29 -0600

    Test reset functionality

--- hw/ch27/tb_cnt.sv
+++ hw/ch27/tb_cnt.sv
@@ -12,7 +12,7 @@ module tb_cnt.sv();
     logic clk, rst, en, up, load;
     logic[3:0] count_in, count;
 
-    cnt CountMod(clk, rst, en, up, load, count_in, count);
+    cnt DUT(clk, rst, en, up, load, count_in, count);
 
     initial begin
         #100ns;
@@ -22,6 +22,19 @@ module tb_cnt.sv();
         end
         #100ns;
     end
-    
+    // test functionality of reset
+    initial begin
+        // initial values for testing
+        rst = 0;
+        en = 0;
+        up = 0;
+        load = 0;
+        // wait 4 clock cycles
+        repeat(4) @(negedge clk);
+        // Issue a reset for a few clock cycles and release the reset
+        rst = 1;
+        repeat(4) @(negedge clk);
+        rst = 0;
+    end
 
 endmodule
