cdce345823218e70c298ee516d8014744258ee5c
eckscott <156133501+eckscott@users.noreply.github.com>
2025-04-03 15:47:11 -0600

    Count up and roll over

--- hw/ch27/tb_cnt.sv
+++ hw/ch27/tb_cnt.sv
@@ -11,7 +11,7 @@ module tb_cnt.sv();
 
     logic clk, rst, en, up, load;
     logic[3:0] count_in, count;
-    int i;
+    int i, errors;
 
     cnt DUT(clk, rst, en, up, load, count_in, count);
 
@@ -32,6 +32,7 @@ module tb_cnt.sv();
         en = 0;
         up = 0;
         load = 0;
+        errors = 0;
         // wait 4 clock cycles
         repeat(4) @(negedge clk);
         // Issue a reset for a few clock cycles and release the reset
@@ -44,18 +45,16 @@ module tb_cnt.sv();
         en = 1;
         up = 1;
         for (i=0; i < 16; i=i+1) begin
-            if (count == i)
-                @(negedge clk);
-            else begin
-                $display("Incorrect count val. Should be '%1d' but is '%1d' at time %0t", i, count, $time);
-                $finish;
+            if (count != i) begin
+                $display("ERROR: Incorrect count val. Should be '%1d' but is '%1d' at time %0t", i, count, $time);
+                errors = errors + 1;
             end
+            @(negedge clk);
         end 
-        if (count == 0)
-            $display("Count from 0 to 15 and roll over back to 0 success!);
+        if (count != 0)
+            $display("ERROR: Failed to roll back to 0. Count should be '0' but is '%1d' at time %0t, count, $time);
         else
-            $display("Failed
-        
+            $display("Test Roll back to 0: SUCCESS");
     end
 
 endmodule
